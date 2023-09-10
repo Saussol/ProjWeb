7df60892c24847c40857c765a26fdc265b31322b
Saussol <60229019+Saussol@users.noreply.github.com>
2023-09-10 18:02:33 +0200

    modif api

--- ApiServeur/ServeurApi.js
+++ ApiServeur/ServeurApi.js
@@ -20,34 +20,6 @@ db.connect((err) => {
   console.log('Connecté à la base de données MySQL');
 });
 
-// Configuration de la route de soumission des scores
-app.post('/scoreSubmissionEndpoint', (req, res) => {
-    const nom = req.body.name;
-    const score = req.body.score;
-
-    // Insérez les données dans la base de données MySQL
-    const sql = 'INSERT INTO scores (nom, score) VALUES (?, ?)';
-    db.query(sql, [nom, score], (err, result) => {
-        if (err) {
-            console.error('Erreur lors de l\'insertion du score : ', err);
-            res.status(500).json({ error: 'Erreur lors de l\'enregistrement du score.' });
-        } else {
-            console.log('Score enregistré avec succès.');
-            res.status(200).json({ message: 'Score enregistré avec succès.' });
-        }
-    });
-});
-
-// Exemple de route pour récupérer des données depuis la base de données
-app.get('/scoreBorad', (req, res) => {
-  const sql = 'SELECT * FROM scores';
-  db.query(sql, (err, result) => {
-    if (err) {
-      throw err;
-    }
-    res.json(result);
-  });
-});
 
 // Démarrage du serveur
 app.listen(port, () => {
