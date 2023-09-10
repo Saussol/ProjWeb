e22fed6006d492af178f6592c211e0f764ecf048
Saussol <60229019+Saussol@users.noreply.github.com>
2023-09-10 19:08:52 +0200

    zae

--- ServeurApi.js
+++ ServeurApi.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/main.html');
 });
 
-app.get('/leaderboard', (req, res) => {
+app.get('/scoreBorad', (req, res) => {
   const query = 'SELECT * FROM scoreBoard ORDER BY score DESC'; // Triez par score décroissant
 
   db.query(query, (err, results) => {
